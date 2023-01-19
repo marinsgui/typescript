5d2086ac7a961050af0225a8d01630741663d20f
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-01-19 11:04:32 -0300

    tipos explícitos

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,11 +1,15 @@
 // string
-let nome = 'João'
+let nome: string = 'João'
 console.log(nome)
 
 // number
-let idade = 26
+let idade: number = 26
 console.log(idade)
 
 // boolean
-let possuiHobbies = true
+let possuiHobbies: boolean = true
 console.log(possuiHobbies)
+
+// tipos explícitos
+let minhaIdade: number = 26
+console.log(minhaIdade)
