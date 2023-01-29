d302242d9e36fe1f889315eb09a1f2dbe4fd7cc7
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-01-29 10:38:53 -0300

    tipo never

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -104,3 +104,21 @@ if (typeof valor === 'number') {
 } else {
     console.log(typeof valor)
 }
+
+// tipo never
+function falha(msg: string): never {
+    throw new Error(msg)
+}
+
+const produto = {
+    nome: 'Sabão',
+    preco: -1,
+    validarProduto() {
+        if(!this.nome || this.nome.trim().length === 0) {
+            falha('Precisa ter um nome')
+        }
+        if(this.preco <= 0) {
+            falha('Preço inválido')
+        }
+    }
+}
