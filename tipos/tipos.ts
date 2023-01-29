78245e2cd918c8ca03f3151276235fb24806378e
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-01-29 14:08:11 -0300

    desafio de tipagem

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -139,3 +139,29 @@ const contato1: Contato = {
     tel2: null
 }
 console.log(contato1.nome, contato1.tel1, contato1.tel2)
+
+// desafio
+type ContaBancaria = {
+    saldo: number,
+    depositar: (valor: number) => void
+}
+let contaBancaria: ContaBancaria = {
+    saldo: 3456,
+    depositar(valor: number): void {
+        this.saldo += valor
+    }
+}
+
+type Correntista = {
+    nome: string,
+    contaBancaria: ContaBancaria,
+    contatos: string[]
+}
+
+let correntista: Correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+}
+correntista.contaBancaria.depositar(3000)
+console.log(correntista)
